44c8dd523e7221f6489413f0d0d71f2b203641c7
Martin Ohmeyer <94917935+ohmy3000@users.noreply.github.com>
2024-10-17 13:32:36 +0200

    receiving test data on Arduino

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+
 void setup() {
-	Serial.begin(9600);
+    Serial.begin(9600);
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+
+	/// TODO: Ard zu B15 schreiben
 }
 
 void loop() {
-	Serial.println("Hello world!");
-	delay(1000);
+    delay(1000);
+
+    uint8_t input = 0x0;
+
+    for (int i = 0; i < 4; i++) {
+        // Lese den Zustand des Pins (2+i)
+        uint8_t pinValue = digitalRead(2 + i);
+        // Setze das entsprechende Bit in input, wenn der Pin HIGH ist
+        if (pinValue == HIGH) {
+            input |= (0x1 << i);
+        }
+    }
+
+    Serial.println(input);
 }
