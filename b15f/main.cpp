b944cf4a4f14b828ff0d1cd3d396205c7835b04c
tomo <49612544+tomo2403@users.noreply.github.com>
2024-10-17 13:32:29 +0200

    sending test data

--- b15f/main.cpp
+++ b15f/main.cpp
@@ -1,8 +1,25 @@
+#include <b15f/b15f.h>
 #include <iostream>
 
 #include "crc.hpp"
 
+using namespace std;
+
 int main()
 {
+    B15F& drv = B15F::getInstance();
+    drv.setRegister(&DDRA, 0x0F);
+
+    drv.setRegister(&PORTA, 0x05);
+
+//    while (true) {
+//        drv.setRegister(&PORTA, 0x00);
+//        cout << "0" << endl;
+//        //drv.delay_ms(500);
+//        drv.setRegister(&PORTA, 0xA0);
+//        cout << "1" << endl;
+//        //drv.delay_ms(500);
+//    }
+
 	return 0;
 }
