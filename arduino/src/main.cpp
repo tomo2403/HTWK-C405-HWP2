b3b8f657d553f4ba42d3b26628867ae36b3866e1
tomo <49612544+tomo2403@users.noreply.github.com>
2024-11-25 12:09:38 +0100

    implemented arduino

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 
+uint8_t lastReceived = 0;
+
+uint8_t readNibble() {
+	// D2 to D5
+    return (PIND & 0b00111100) >> 2;
+}
+
+void writeNibble(uint8_t nibble){
+	// D6 to D9
+	PORTD = (PORTD & 0b11000011) | ((nibble & 0b00001111) << 2);
+}
+
 void setup()
 {
 	Serial.begin(57600);
@@ -7,5 +19,13 @@ void setup()
 
 void loop()
 {
+	uint8_t received = readNibble();
+	if(received != lastReceived){
+		Serial.write(received);
+		lastReceived = received;
+	}
 
+	if (Serial.available()){
+		writeNibble(Serial.read());
+	}
 }
