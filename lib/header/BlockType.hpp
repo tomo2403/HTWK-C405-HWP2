bc2e3e95949a77a4dc84036355897e934f85681c
martin <mo_apps@tuta.io>
2024-11-25 16:08:20 +0100

    create interface for decoder events

---
+++ lib/header/BlockType.hpp
@@ -0,0 +1,5 @@
+enum BlockType
+{
+    dataBlock,
+    controlBlock
+};
---
+++ lib/header/IDecoderObserver.hpp
@@ -0,0 +1,11 @@
+#include "BlockType.hpp"
+
+class IDecoderObserver
+{
+public:
+    virtual void beginBlockReceived(const BlockType &blockType) = 0;
+    virtual void endBlockReceived(const BlockType &blockType) = 0;
+    virtual void iAmReadyReceived() = 0;
+    virtual void everythingSentReceived() = 0;
+    virtual void closeConnectionReceived() = 0;
+};
