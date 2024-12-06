91239ee0a70484e38aaba4fa8f90318bb598e4ce
ohmy3000 <94917935+ohmy3000@users.noreply.github.com.>
2024-12-06 21:17:41 +0100

    set up test scenario

--- b15f/B15Fake.hpp
+++ b15f/B15Fake.hpp
@@ -1,16 +1,29 @@
 #pragma once
 #include <cstdint>
+#include <iomanip>
+#include <vector>
+#include <iostream>
 
 class B15Fake
 {
+private:
+    std::vector<uint8_t> readData;
+    int readDataOffset = 0;
+
 public:
+    B15Fake(const std::vector<uint8_t> &readData) : readData(readData)
+    {}
+
     uint8_t getRegister(volatile uint8_t *adr)
     {
-        return 0x00;
+        uint8_t value = readData.at(readDataOffset++);
+        std::cout << "getReg: " << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(value) << std::endl;
+        return value;
     };
 
     void setRegister(volatile uint8_t *adr, uint8_t val)
     {
+        std::cout << "setReg: " << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(val) << std::endl;
         return;
     };
 };
--- b15f/main.cpp
+++ b15f/main.cpp
@@ -8,20 +8,28 @@
 
 int main()
 {
-	#ifdef DEBUG_MODE
-		B15Fake drv = B15Fake();
-	#else
-		B15F& drv = B15F::getInstance();
-	#endif
-
 	Decoder decoder = Decoder();
 	Encoder encoder = Encoder();
+
+	#ifdef DEBUG_MODE
+		std::vector<uint8_t> incommingData =
+		{};
+
+		std::vector<uint8_t>  outgoingData =
+		{};
+
+
+		B15Fake drv = B15Fake(incommingData);
+		B15Sender sender = B15Sender(drv, decoder, encoder, outgoingData);
+	#else
+		B15F& drv = B15F::getInstance();
+		B15Sender sender = B15Sender(drv, decoder, encoder, ioManager::getBinaryInput());
+	#endif
 
 	// 7-4 inbound, 3-0 outbound
 	drv.setRegister(&DDRA, 0x0F);
 	
 	B15Receiver receiver = B15Receiver(drv, decoder, encoder);
-	B15Sender sender = B15Sender(drv, decoder, encoder, ioManager::getBinaryInput());
 
 	while(true)
 	{
