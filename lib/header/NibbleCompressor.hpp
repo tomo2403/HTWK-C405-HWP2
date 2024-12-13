a6bea695db6ea8533f45c214954d0bd3e8893d9a
ohmy3000 <94917935+ohmy3000@users.noreply.github.com.>
2024-12-13 09:19:03 +0100

    implement data-structure to recompress nibbles to bytes

---
+++ lib/header/NibbleCompressor.hpp
@@ -0,0 +1,16 @@
+#include "../lib.hpp"
+
+class NibbleCompressor
+{
+private:
+    uint8_t buffer;
+    std::vector<uint8_t> dataVector_byte;
+    bool nextNibbleIsHighNibble;
+
+public:
+    NibbleCompressor(const size_t &size = 0);
+    
+    void pushBack(const uint8_t &nibble);
+
+    std::vector<uint8_t> getData();
+};
