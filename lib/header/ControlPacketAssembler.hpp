3cede5cb2309b179f2a3a950f354f107822f74c0
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-16 13:30:33 +0100

    implement control-packet assembly

---
+++ lib/header/ControlPacketAssembler.hpp
@@ -0,0 +1,30 @@
+#include "CRC.hpp"
+
+enum Flags : uint8_t
+{
+	/** @brief The sender has delivered all data and is ready to close the connection. */
+	TRANSFER_FINISHED = 1 << 0, // Bit 0
+	/** @brief The sender will close the connection and is not receiving any new packets. */
+	CLOSE_CONNECTION = 1 << 1, // Bit 1
+	/** @brief The receiver requests a resend of the packet with id provided. */
+	RESEND = 1 << 2, // Bit 2
+	/** @brief The sender wants to establish a connection. */
+	CONNECT = 1 << 3, // Bit 3
+	/** @brief The receiver confirms that it has received the packet with id provided. */
+	RECEIVED = 1 << 4, // Bit 4
+};
+
+class ControlPacketAssembler
+{
+private:
+    
+    ControlPacketAssembler() = delete;
+
+    static const void packetAssembly_addId(std::vector<uint8_t> &packet, const uint16_t &id);
+    static const void packetAssembly_addFlags(std::vector<uint8_t> &packet, const uint8_t &flags);
+    static const void packetAssembly_addCrc(std::vector<uint8_t> &packet);
+    
+public:
+    
+    static const std::vector<uint8_t> assemble(const uint8_t &flags, const uint32_t &packetId);
+};
---
+++ lib/implementation/ControlPacketAssembler.cpp
@@ -0,0 +1,36 @@
+#include "../header/ControlPacketAssembler.hpp"
+
+const void ControlPacketAssembler::packetAssembly_addId(std::vector<uint8_t> &packet, const uint16_t &id)
+{
+    packet.reserve(2);
+    packet.push_back(static_cast<uint8_t>(id >> 8));
+    packet.push_back(static_cast<uint8_t>(id & 0x0F));
+}
+
+const void ControlPacketAssembler::packetAssembly_addFlags(std::vector<uint8_t> &packet, const uint8_t &flags)
+{
+    packet.push_back(flags);
+}
+
+const void ControlPacketAssembler::packetAssembly_addCrc(std::vector<uint8_t> &packet)
+{
+    const uint32_t crc = CRC::calculateCRC(packet);
+    
+    packet.reserve(4);
+
+    // 'i' is an element of {3, 2, 1, 0}
+    for (int8_t i = 3; i >= 0; --i)
+    {
+        packet.push_back(static_cast<uint8_t>(crc >> (i*8)));
+    }
+}
+
+const std::vector<uint8_t> ControlPacketAssembler::assemble(const uint8_t &flags, const uint32_t &packetId)
+{
+    std::vector<uint8_t> packet;
+    packetAssembly_addId(packet, packetId);
+    packetAssembly_addFlags(packet, flags);
+    packetAssembly_addCrc(packet);
+
+    return packet;
+}
