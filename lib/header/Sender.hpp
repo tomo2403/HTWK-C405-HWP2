52f78a949b9fa847a1d565423c83220898f7ab76
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-17 00:46:52 +0100

    begin sender implementation

--- lib/header/Sender.hpp
+++ lib/header/Sender.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
 #include "AtomicQueue.hpp"
+#include "InterthreadNotification.hpp"
 
 class Sender
 {
 private:
+    static const uint8_t secondsUntilTimeout;
 
-    AtomicQueue<uint8_t>* outgoingQueue;;
+    AtomicQueue<uint8_t>* datastreamQueue_outgoing;
+    AtomicQueue<InterthreadNotification>* notificationQueue_incoming;
 
+    void processNotification(const InterthreadNotification &notification);
+
+    void encoderCallback_onDataBlockSent();
 
 public:
-    Sender(AtomicQueue<uint8_t>* outgoingQueue);
+    Sender(AtomicQueue<uint8_t>* datastreamQueue_outgoing, AtomicQueue<InterthreadNotification>* notificationQueue_incoming);
 
     // To start in another thread
     void send();
--- lib/implementation/Sender.cpp
+++ lib/implementation/Sender.cpp
@@ -1,6 +1,27 @@
 #include "../header/Sender.hpp"
 
-Sender::Sender(AtomicQueue<uint8_t>* outgoingQueue)
-    : outgoingQueue(outgoingQueue)
+Sender::Sender(AtomicQueue<uint8_t>* datastreamQueue_outgoing,AtomicQueue<InterthreadNotification>* notificationQueue_incoming)
+    : datastreamQueue_outgoing(datastreamQueue_outgoing), notificationQueue_incoming(notificationQueue_incoming)
 { 
+}
+
+void processNotification(const InterthreadNotification &notification)
+{
+
+}
+
+void Sender::send()
+{
+    // TODO: How to terminate program?
+    while(true)
+    {
+        if (!notificationQueue_incoming->empty())
+        {
+            processNotification(notificationQueue_incoming->wait_and_pop());
+        }
+        else
+        {
+            // push data nibble into outQueue
+        }
+    }
 }
