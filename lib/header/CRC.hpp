529f5643645d6867b3bcbed0b68b4fac91de6b48
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-16 12:40:23 +0100

    translate CRC Documentation to English

--- lib/header/CRC.hpp
+++ lib/header/CRC.hpp
@@ -4,39 +4,39 @@
 #include <cstdint>
 
 /**
- * @brief Klasse zum Generieren und Validieren eines CRC-Wertes.
+ * @brief Class for generating and validating a CRC value.
  *
  * @details
- * Die Klasse unterstützt (aktuell) nur die Berechnung "gewöhnlicher" CRCs. Das TReflektieren der Eingabe oder Ausgabe
- * ist nicht (noch) nicht möglich. Bisher sind nur 32-Bit CRCs berechenbar.
+ * The class currently supports only the calculation of "ordinary" CRCs. Reflecting the input or output
+ * is not (yet) possible. Currently, only 32-bit CRCs can be calculated.
  */
 class CRC
 {
 public:
-	/**
-	 * @brief Berechnet 32-Bit CRC-Wert über gegebene Daten.
-	 *
-	 * @details
-	 * Die Brechung erfolgt über die Nachahmung eines (Hardware-)Schieberegisters. Der CRC wird inplace und
-	 * ohne zusätzlichen Speicherbedarf berechnet. Jeder Funktionsaufruf führt zu einer neuen Berechnung.
-	 *
-	 * @param data Daten, über welche der CRC-Wert berechnet werden soll.
-	 * @return uint32_t; (Nur) 32bit CRC-Wert der Daten.
-	 */
-	[[nodiscard]] uint32_t calculateCRC(const std::vector<uint8_t> &data) const;
+    /**
+     * @brief Calculates a 32-bit CRC value over the given data.
+     *
+     * @details
+     * The calculation is performed by mimicking a (hardware) shift register. The CRC is calculated in place
+     * and without additional memory requirements. Each function call results in a new calculation.
+     *
+     * @param data Data over which the CRC value should be calculated.
+     * @return uint32_t; (Only) 32-bit CRC value of the data.
+     */
+    [[nodiscard]] uint32_t calculateCRC(const std::vector<uint8_t> &data) const;
 
-	void attachCRC(std::vector<uint8_t> &data) const;
+    void attachCRC(std::vector<uint8_t> &data) const;
 
-	/**
-	 * @brief Validiert CRC-Wert.
-	 * @param data Daten, zu welchen ein CRC-Wert validiert werden soll.
-	 * @param receivedCRC CRC-Wert, welcher zu den gegebenen Daten vorliegt.
-	 * @see calculateCRC(myVector8) - Wird zur Berechnung des CRC-Wertes genutzt, mit dem der gegebene CRC-Wert verglichen wird.
-	 * @return bool; TRUE, falls CRC-Wert zu Daten passt; FALSE, sonst
-	 */
-	[[nodiscard]] bool validateCRC(const std::vector<uint8_t> &data, uint32_t receivedCRC) const;
+    /**
+     * @brief Validates a CRC value.
+     * @param data Data for which a CRC value should be validated.
+     * @param receivedCRC CRC value that is associated with the given data.
+     * @see calculateCRC(myVector8) - Used to calculate the CRC value that will be compared with the given CRC value.
+     * @return bool; TRUE if the CRC value matches the data; FALSE otherwise.
+     */
+    [[nodiscard]] bool validateCRC(const std::vector<uint8_t> &data, uint32_t receivedCRC) const;
 
 private:
-	uint32_t polynomial = 0x04c11db7;
-	uint32_t initialValue = 0xffffffff;
+    uint32_t polynomial = 0x04c11db7;
+    uint32_t initialValue = 0xffffffff;
 };
