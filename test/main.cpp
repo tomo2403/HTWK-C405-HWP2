0336219dcfb678e2f2b073aeef5ce53b02073470
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-17 00:47:15 +0100

    clear test-main

--- test/main.cpp
+++ test/main.cpp
@@ -3,27 +3,9 @@
 #include "../lib/header/DataPacketAssembler.hpp"
 #include "../lib/header/DataPacketDisassembler.hpp"
 #include "../lib/header/ControlPacketAssembler.hpp"
+#include "../lib/header/Encoder.hpp"
+#include "../lib/header/Timer.hpp"
 
-int main() {
-    std::vector<uint8_t> testData = {9, 3, 7, 1, 43, 4};
-    std::vector<uint8_t> control = ControlPacketAssembler::assemble(Flags::CONNECT, 5);
-    
-    DataPacketAssembler assembler(testData);
-    
-    uint16_t testPacketId = 0;
-
-    std::vector<uint8_t> packet = assembler.getPacket(testPacketId);
-
-    DataPacketDisassembler disassembler;
-
-    bool result = disassembler.processPacket(packet, testPacketId);
-
-    if (result)
-    {
-        std::cout << "Success" << std::endl;
-    } else {
-        std::cout << "Fail" << std::endl;
-    }
-
+int main() {    
     return 0;
 }
