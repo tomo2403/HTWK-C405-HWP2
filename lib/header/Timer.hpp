141fb9bd8904f4232104546f20e3faae56b97d17
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-17 00:38:14 +0100

    implemented timer class

---
+++ lib/header/Timer.hpp
@@ -0,0 +1,20 @@
+#include <chrono>
+
+class Timer
+{
+private:
+    bool isRunning;
+    std::chrono::high_resolution_clock::time_point start_time_point;
+    std::chrono::high_resolution_clock::time_point end_time_point;
+
+public:
+
+    Timer();
+
+    void start();
+
+    void stop();
+
+    // uint: seconds
+    double elapsed();
+};
---
+++ lib/implementation/Timer.cpp
@@ -0,0 +1,26 @@
+#include "../header/Timer.hpp"
+
+Timer::Timer()
+    : isRunning(false)
+{
+}
+
+void Timer::start()
+{
+    start_time_point = std::chrono::high_resolution_clock::now();
+    isRunning = true;
+}
+
+void Timer::stop()
+{
+    end_time_point = std::chrono::high_resolution_clock::now();
+    isRunning = false;
+}
+
+double Timer::elapsed()
+{
+    if (isRunning) {
+        end_time_point = std::chrono::high_resolution_clock::now();
+    }
+    return std::chrono::duration<double>(end_time_point - start_time_point).count();
+}
