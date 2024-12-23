f1ebdf9d7551c07b3e221144e0a8a60d6ee544f3
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-24 00:33:50 +0100

    improved implementation of AtomicBoolean

--- lib/header/AtomicBoolean.hpp
+++ lib/header/AtomicBoolean.hpp
@@ -1,22 +1,14 @@
 #pragma once
 
-#include <iostream>
 #include <atomic>
-#include <thread>
-#include <chrono>
 
 class AtomicBoolean {
+
 private:
     std::atomic<bool> value;
 
 public:
-    AtomicBoolean() : value(false) {}
-
-    void set(bool newValue) {
-        value.store(newValue, std::memory_order_relaxed);
-    }
-
-    bool get() const {
-        return value.load(std::memory_order_relaxed);
-    }
+    AtomicBoolean();
+    operator bool() const;
+    AtomicBoolean& operator=(bool newValue);
 };
