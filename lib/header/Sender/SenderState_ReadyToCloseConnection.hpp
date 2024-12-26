77f0c7448a7db723d79bd9ea430726dbf169ddf9
ohmy3000 <martin.ohmeyer@tuta.io>
2024-12-26 16:18:34 +0100

    sender closes connection after timeout of 60s, when all data is sent

--- lib/header/Sender/SenderState_ReadyToCloseConnection.hpp
+++ lib/header/Sender/SenderState_ReadyToCloseConnection.hpp
@@ -4,6 +4,9 @@
 
 class SenderState_ReadyToCloseConnection final : public SenderState
 {
+private:
+    Timer timeSinceLastReceivedPacket_timer;
+
 public:
     SenderState_ReadyToCloseConnection(Sender* sender, SenderResources* resources);
 
--- lib/implementation/Sender/SenderState_ReadyToCloseConnection.cpp
+++ lib/implementation/Sender/SenderState_ReadyToCloseConnection.cpp
@@ -8,11 +8,14 @@ SenderState_ReadyToCloseConnection::SenderState_ReadyToCloseConnection(Sender *s
 {
     if (resources->globalTimer.running())
         resources->globalTimer.stop();
+
+    timeSinceLastReceivedPacket_timer.start();
 }
 
 void SenderState_ReadyToCloseConnection::processNotification()
 {
     const InterthreadNotification notification = resources->notificationQueue_incoming->wait_and_pop();
+    timeSinceLastReceivedPacket_timer.start();
 
     switch (notification.type)
     {
@@ -41,8 +44,16 @@ void SenderState_ReadyToCloseConnection::processDataQueueIsEmpty()
     const std::vector<uint8_t> controlBlock = ControlPacketAssembler::assemble(Flag::CLOSE_CONNECTION, 0);
     resources->encoder.pushBlock(BlockType::controlBlock, controlBlock);
 
+    // Shutdown is already scheduled.
     if (resources->shutDownAsap)
     {
         sender->shutDown();
+    }
+
+    // The time since the last received notification, and therefore control-Packet, exceeds 60s.
+    if (timeSinceLastReceivedPacket_timer.elapsed() >= 60)
+    {
+        sender->shutDown();
+        Logger(WARNING) << "Waiting to close the connection but did not receive any packets for 60 seconds. Closing connection due to timeout.\n";
     }
 }
