366f4ae61ef0a28843584beb60556b7ec13e01cd
tomo <49612544+tomo2403@users.noreply.github.com>
2024-12-27 20:48:20 +0100

    enhanced timer

--- lib/implementation/Timer.cpp
+++ lib/implementation/Timer.cpp
@@ -7,19 +7,26 @@ Timer::Timer()
 
 void Timer::start()
 {
-    start_time_point = std::chrono::high_resolution_clock::now();
-    isRunning = true;
+    if (!running())
+    {
+        start_time_point = std::chrono::high_resolution_clock::now();
+        isRunning = true;
+    }
 }
 
 void Timer::stop()
 {
-    end_time_point = std::chrono::high_resolution_clock::now();
-    isRunning = false;
+    if (running())
+    {
+        end_time_point = std::chrono::high_resolution_clock::now();
+        isRunning = false;
+    }
 }
 
 double Timer::elapsed()
 {
-    if (isRunning) {
+    if (isRunning)
+    {
         end_time_point = std::chrono::high_resolution_clock::now();
     }
     return std::chrono::duration<double>(end_time_point - start_time_point).count();
